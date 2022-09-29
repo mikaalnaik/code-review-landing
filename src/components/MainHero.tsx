b4f41abab3cdd827853c13bdc9cc6f5ab3a84aae
Mikaal Naik <mikaalnaik@gmail.com>
2022-09-29 17:48:46 -0400

    Copy changes

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -13,9 +13,13 @@ const MainHero = ({ isVariant }: Props) => {
   const { mainHero } = config;
 
   const title = isVariant
-    ? 'Grow your landscaping business'
-    : 'Make up to $60K';
+    ? 'Grow your home maintenaince business'
+    : 'Kickstart a new home maintenaince business';
   const subtitle = isVariant ? 'instantly with HomeHero' : 'with HomeHero.';
+
+  const mainBody = isVariant
+    ? 'Instantly grow your client list and take customer support and payroll off your plate.'
+    : "It's never been easier to build your own home maintenaince business. Let HomeHero take care of your clients, payroll and scheduling.";
 
   const router = useRouter();
 
@@ -31,7 +35,7 @@ const MainHero = ({ isVariant }: Props) => {
           <span className={`block text-primary xl:inline`}>{subtitle}</span>
         </h1>
         <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-          {mainHero.description}
+          {mainBody}
         </p>
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
