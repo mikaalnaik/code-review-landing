bf9f146617269d61ee426db098d35408d894575c
Mikaal Naik <mikaalnaik@gmail.com>
2022-09-29 21:02:53 -0400

    Fix spelling

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -13,13 +13,13 @@ const MainHero = ({ isVariant }: Props) => {
   const { mainHero } = config;
 
   const title = isVariant
-    ? 'Grow your home maintenaince business'
-    : 'Kickstart a new home maintenaince business';
+    ? 'Grow your home maintenance business'
+    : 'Kickstart a new home maintenance business';
   const subtitle = isVariant ? 'instantly with HomeHero' : 'with HomeHero.';
 
   const mainBody = isVariant
     ? 'Instantly grow your client list and take customer support and payroll off your plate.'
-    : "It's never been easier to build your own home maintenaince business. Let HomeHero take care of your clients, payroll and scheduling.";
+    : "It's never been easier to build your own home maintenance business. Let HomeHero take care of your clients, payroll and scheduling.";
 
   const router = useRouter();
 
